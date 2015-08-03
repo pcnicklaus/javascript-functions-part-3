2a94180f1b259a8f0f64b3df4b3a132c9b621f9d
Patrick <pcnicklaus@gmail.com>
2015-08-03 14:16:24 -0600

    Few more solved, two to finish up

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ console.log("sanity check!");
 
 // Define a function called getStudentName that takes a single object as an argument and returns the value of the name property/key.
 
+// A)
 var student = {name: 'Michael', age: 27 }
 // getStudentName(name) // => Michael
 
@@ -10,25 +11,16 @@ function getStudentName(object) {
   console.log(object.name);
   return object.name;
 }
-
 getStudentName(student);
 
-// student.name = Michael?
-// student.age = 27?
-
+// B)
 var student = {name: 'Michael', age: 27 }
 // getStudentName(name) // => Michael
 
 function getStudentName(object) {
-  var x = student.object;
-  console.log(x);
-  console.log(student.object);
-  return student.object;
+  return student.name;
 }
-
 getStudentName(name);
-
-// what the fuck am i doing wrong above????
 
 
 // 2 - Working
@@ -102,6 +94,7 @@ function removeCharacter(string, character) {
   var string2 = string.split(character).join("");
   console.log(string);
   console.log(string2);
+}
 
   // what's wrong with my logic here? why wasn't it even entering into the loop???
   // for (var i = 0; i < string2.length; i++) {
@@ -117,9 +110,9 @@ function removeCharacter(string, character) {
   //   }
   //   console.log(string2);
   // }
-}
 
-// Not working
+
+// Working
 // 6. Define a function called ouputObject that takes an object as an argument and returns each key-value pair in the following format 'key .
 //Object.keys and ages.john
 // for in loop = research it
@@ -130,7 +123,7 @@ function outputObject (object) {
   var txt = "";
   var x;
   for (x in ages) {
-      txt += x.key + " is " + ages[x];
+      txt += x + " is " + ages[x] + ", ";
       console.log(txt);
   }
 }
@@ -211,14 +204,44 @@ getVowels('javascripting'); // => ['a', 'i']
 var first = [];
 var second = [];
 
-var in = ['m', 'm', 'n', 'n', 's', 's']
-var in2 = ['m', 'm', 'm', 'n', 's', 's']
+var in1 = ['m', 'm', 'n', 'n', 's', 's']
 
 captureTwins(['m', 'm', 'n', 'n', 's', 's']) // => true
 captureTwins(['m', 'm', 'm', 'n', 's', 's']) // => false
+captureTwins(['m', 'm', 'n', 'n', 's', 's']) // => true
+
+captureTwins(['m', 'm', 'm', 'n', 's', 's']) // => false
+
+function captureTwins(array) {
+  var pairsArray = [];
+  var x = [];
+  var q = 0;
+  for (var i = 0; i < array.length + 1; i++) {
+    var x = array.splice(0,2);
+    console.log(x)
+    pairsArray.push(x);
+    console.log(pairsArray);
+  }
+  for (var j = 0; j < pairsArray.length; j++) {
+    if (pairsArray[j][0] !== pairsArray[j][1]) {
+      console.log(pairsArray[j] + "= [j]")
+      console.log(pairsArray[j][0] + "= [j][0]");
+      console.log(pairsArray[j][1] + "= [j][1]");
+      q++;
+      return false;
+    }
+    // else {
+    //   if (q >= 0) {
+    //     return true;
+    //   }
+    // }
+  }
+}
 
 
 
+var in2 = ['m', 'm', 'm', 'n', 's', 's']
+captureTwins(['m', 'm', 'm', 'n', 's', 's']) // => false
 
 
 // function captureTwins(array) {
@@ -272,28 +295,72 @@ testBooleanLogic([false, false, false]) // =>  false
 //    start over....
 
 
+// getUniqueValues(['m', 'n', 'm', 'r', 'r', 's']) // => ['m', 'n', 'r', 's']
+// getUniqueValues(['michael', 'ben', 'kerry', 'ben']) // => ['michael', 'ben', 'kerry']
+
+
+// function getUniqueValues(array) {
+//   var originals = [];
+//   originals = array.splice(0,1);
+//   console.log(array);
+//   console.log(originals + " originals Beginning");
+//   for (var i = 0; i < array.length; i++) {
+//      for (var j = 0; j < originals.length; j++) {
+//         var r = array[i];
+//         if ( array[i] === originals[j]) {
+//           debugger;
+//           array.shift();
+//           console.log(array + " array in if");
+//       } else {
+//           originals.push(r);
+//           array.shift();
+//           // holder += array.slice(0,1);
+//           debugger;
+//           console.log(i + " i")
+//           console.log(originals + " originals else");
+//       }
+//     }
+//   }
+// }
+
 getUniqueValues(['m', 'n', 'm', 'r', 'r', 's']) // => ['m', 'n', 'r', 's']
 getUniqueValues(['michael', 'ben', 'kerry', 'ben']) // => ['michael', 'ben', 'kerry']
 
+
 function getUniqueValues(array) {
-  var originals, holder = [];
-  holder = array.slice(0,1);
-  console.log(originals);
-  for (var i = 0; i < array.length; i++) {
-     for (var j = 0; j < holder.length; j++) {
-        if ( array[i] === holder[j]) {
-          array.slice(0,1);
-          console.log(array + " array in if");
+  var copy = array.splice(0,1);
+  var originals = [];
+  // originals = array.splice(0,1);
+  // console.log(array);
+  // console.log(originals + " originals Beginning");
+  for (var i = 0; i < copy.length; i++) {
+    console.log(copy.length)
+     for (var j = 0; j < array.length; j++) {
+        var r = array[j];
+        console.log(r + " = r value")
+        if ( copy[i] === array[j] ) {
+          console.log(array[j] + " arrayI If");
+          console.log(copy[i] + " copyJ If");
+          array.shift();
+          console.log(array + " array contents in If");
+          console.log(copy + " copy contents in If");
+
       } else {
-          originals.push(array[i]);
-          array.slice(0,1);
+          originals.push(r);
+          console.log(originals + "originals  contents in Else")
+          array.shift();
+          console.log(array + "array contents in Else")
+          var w = array.splice(0,1);
+          copy.push(w);
+          console.log(copy + " copy contents in Else")
+          // holder += array.slice(0,1);
+          debugger;
           console.log(i + " i")
           console.log(originals + " originals else");
       }
     }
   }
 }
-
 
 
 
